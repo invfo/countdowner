fab20f2c87e1d10098fa50439e1e843213c31cd8
Darya Talanina <dtalanina@gmail.com>
2019-04-01 21:55:15 +0200

    Basic app with countdown and event name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
+
+const padWithZeroes = number => {
+  if (number <= 9) {
+    return "0" + String(number);
+  }
+  return number;
+}
+
+const formatRemainingTime = (millisecondsRemaining) => {
+  const days = Math.floor(millisecondsRemaining / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (millisecondsRemaining - days * 1000 * 60 * 60 * 24) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((millisecondsRemaining - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor((millisecondsRemaining - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000);
+
+  hours = padWithZeroes(hours);
+  minutes = padWithZeroes(minutes);
+  seconds = padWithZeroes(seconds);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+const Countdown = ({eventDate}) => {
+  const [millisecondsUntilEvent, setMillisecondsUntilEvent] = useState(
+    eventDate - new Date()
+  );
+
+  useEffect(() => {
+    const countdown = setInterval(
+      () => {setMillisecondsUntilEvent(eventDate - new Date())}
+      , 1000
+    );
+
+    return () => {clearInterval(countdown)}
+  });
+
+  const timeRemaining = formatRemainingTime(millisecondsUntilEvent);
+  return (
+    <div>
+      {timeRemaining.days !== 0 ? timeRemaining.days + " jour" + (timeRemaining.days > 1 ? "s" : ""): ""}&nbsp;
+      {timeRemaining.hours}:
+      {timeRemaining.minutes}:
+      {timeRemaining.seconds}
+    </div>
+  )
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>Weekend à Bourboule</div>
+        <Countdown
+          eventDate={new Date(2019, 3, 12, 9, 0, 0)}
+        />
       </div>
     );
   }
